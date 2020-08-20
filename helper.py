9f324b59274c165a2b344741220b58bd71ec7e40
ArvinZhuang <shengyaozhuang@gmail.com>
2020-08-21 09:45:31 +1000

    update helper

--- helper.py
+++ helper.py
@@ -1,4 +1,6 @@
 import json
+import os
+
 from tqdm import tqdm
 
 """
@@ -92,3 +94,21 @@ def getDocumentContent(CONF, dic):
             else:
                 continue
     return dic
+
+
+def results_to_trecrun(folder_path, output, runid):
+    # combine res files in a folder into a single trec run file, ready to submit.
+    files = os.listdir(folder_path)
+    trecrun_file = open(output, "a")
+
+    for f in files:
+        if f == '.DS_Store':
+            pass
+        else:
+            file_path = folder_path+'/'+f
+            with open(file_path) as f:
+                for line in f:
+                    line = line.strip()
+                    topicid, docid, rank, score = line.split("\t")
+                    line = "{} Q0 {} {} {} {}".format(topicid, docid, rank, score, runid)
+                    trecrun_file.write(line+"\n")

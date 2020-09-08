e886fd982a43338cada2e062d1504581e01d258f
ArvinZhuang <shengyaozhuang@gmail.com>
2020-09-08 17:12:19 +1000

    update

--- scripts/convert_model_run_to_trec.py
+++ scripts/convert_model_run_to_trec.py
@@ -18,11 +18,11 @@ out_path = args.output_path
 with open(bm25_run_path, "r") as bm25_file, open(model_run_path, "r") as model_run_file, open(out_path, "a+") as out_file:
     bm25_lines = bm25_file.readlines()
     model_lines = model_run_file.readlines()
-    for ind, line in enumerate(tqdm(bm25_lines)):
+    for ind, line in enumerate(tqdm(model_lines)):
         if bm25_format == 'trec':
-            qid, _, wid, _, _, _ = line.rstrip().split(" ")
+            qid, _, wid, _, _, _ = bm25_lines[ind].rstrip().split(" ")
         else:
-            qid, wid, _, _ = line.rstrip().split("\t")
-        new_line = f'{qid}\t{wid}\t{1}\t{model_lines[ind].rstrip()}\n'
+            qid, wid, _, _ = bm25_lines[ind].rstrip().split("\t")
+        new_line = f'{qid}\t{wid}\t{1}\t{line.rstrip()}\n'
         out_file.write(new_line)
     print("Done!")

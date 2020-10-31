00ee3496cb2abb9ab70bd89add78eb58b60ca9f4
Hang Li <cecillll.lee@gmail.com>
2020-10-31 19:13:21 +1000

    update codebase

--- scripts/gainloss_plot.py
+++ scripts/gainloss_plot.py
@@ -6,39 +6,59 @@ from trectools import TrecRes
 from collections import OrderedDict
 
 if __name__ == "__main__":
-    if len(sys.argv) < 5:
-        print("not enough arguments specified")
-        sys.exit(1)
-    fname1 = sys.argv[1]
-    fname2 = sys.argv[2]
-    measure = sys.argv[3]
-    output = sys.argv[4]
+    # if len(sys.argv) < 5:
+    #     print("not enough arguments specified")
+    #     sys.exit(1)
+    # fname1 = sys.argv[1]
+    # fname2 = sys.argv[2]
+    # measure = sys.argv[3]
+    # output = sys.argv[4]
 
-    ev1 = TrecRes(fname1)
-    ev2 = TrecRes(fname2)
+    files = [
+                [
+                    "/Users/hangli/ielab/GPT_Ranker/images/bm25_T5QLM-ndcg.10.eval",
+                    "/Users/hangli/ielab/GPT_Ranker/images/bm25-ndcg.10.eval",
+                    "BM25+QLM-T5 and BM25"
+                ],
+                [
+                    "/Users/hangli/ielab/GPT_Ranker/images/qld_t5-ndcg.10.eval",
+                    "/Users/hangli/ielab/GPT_Ranker/images/qld-ndcg.10.eval",
+                    "QLM-D+QLM-T5 and QLM-D"
+                ],
+                [
+                    "/Users/hangli/ielab/GPT_Ranker/images/qljm_t5-ndcg.10.eval",
+                    "/Users/hangli/ielab/GPT_Ranker/images/qljm-ndcg.10.eval",
+                    "QLM-JM+QLM-T5 and QLM-JM"
+                ]
+            ]
 
-    r1 = ev1.get_results_for_metric(measure)
-    r2 = ev2.get_results_for_metric(measure)
+    fig, axes = plt.subplots(nrows=1, ncols=3, sharey=True, figsize=(18, 4.5))
 
-    ind = np.arange(len(r1))
+    for set_num, paths in enumerate(files):
 
-    # INSERT CODE TO CREATE THE VISUALISATION HERE.
-    # HINT: https://docs.scipy.org/doc/numpy/reference/generated/numpy.subtract.html
+        ev1 = TrecRes(paths[0])
+        ev2 = TrecRes(paths[1])
 
-    fname1 = fname1.rsplit("/", 1)[1]
-    fname2 = fname2.rsplit("/", 1)[1]
+        r1 = ev1.get_results_for_metric('ndcg_cut_10')
+        r2 = ev2.get_results_for_metric('ndcg_cut_10')
 
-    fname1 = fname1.rsplit(".", 1)[0]
-    fname2 = fname2.rsplit(".", 1)[0]
+        ind = np.arange(len(r1))
 
-    r1 = OrderedDict(sorted(r1.items(), key=lambda t: t[0]))
-    r2 = OrderedDict(sorted(r2.items(), key=lambda t: t[0]))
+        # INSERT CODE TO CREATE THE VISUALISATION HERE.
+        # HINT: https://docs.scipy.org/doc/numpy/reference/generated/numpy.subtract.html
 
-    plt.xticks(ind, list(r1.keys()), rotation="vertical")
-    plt.bar(ind, list(np.subtract(r2.values(), r1.values())))
-    plt.ylim(-0.5, 0.5)
-    plt.title('{} and {}'.format(fname1, fname2))
-    plt.ylabel(measure)
-    plt.tight_layout()
+        r1 = OrderedDict(sorted(r1.items(), key=lambda t: t[0]))
+        r2 = OrderedDict(sorted(r2.items(), key=lambda t: t[0]))
+
+        gains = sorted(list(np.subtract(list(r1.values()), list(r2.values()))), reverse=True)
+
+        if set_num == 0:
+            axes[set_num].set_ylabel("nDCG@10 Gain", fontsize=17)
+        axes[set_num].bar(ind, gains)
+        axes[set_num].set_ylim([-1.1, 1.1])
+        axes[set_num].set_title(paths[2], fontsize=17)
+        axes[set_num].tick_params(axis='y', labelsize=14)
+        axes[set_num].tick_params(axis='x', labelsize=14)
+    plt.subplots_adjust(left=0.07, bottom=0.1, right=0.98, top=0.9, wspace=0.06)
+    plt.savefig('/Users/hangli/ielab/GPT_Ranker/images/gain_comparison.png')
     # plt.show()
-    plt.savefig(output)
--- scripts/rank_comparison.py
+++ scripts/rank_comparison.py
@@ -8,75 +8,101 @@ from collections import OrderedDict
 # python3 rank_comparison.py topK baseline.res better.res relevant.qrels
 
 if __name__ == '__main__':
-    if len(sys.argv) != 5:
-        print("not enough arguments specified")
-        sys.exit(1)
-    topK = int(sys.argv[1])
-    fname1 = sys.argv[2]
-    fname2 = sys.argv[3]
-    fname3 = sys.argv[4]
+    # if len(sys.argv) != 5:
+    #     print("not enough arguments specified")
+    #     sys.exit(1)
+    # topK = int(sys.argv[1])
+    # fname1 = sys.argv[2]
+    # fname2 = sys.argv[3]
+    # fname3 = sys.argv[4]
+    
+    files = [
+                [
+                    "/Users/hangli/ielab/GPT_Ranker/traditional/rerank/t5_rerank_BM25_1000-1_lower.trec.res",
+                    "/Users/hangli/ielab/GPT_Ranker/data/pass_rerank/runs/dev/run.msmarco-passage.dev.small.trec.res",
+                    "/Users/hangli/ielab/GPT_Ranker/data/pass_rerank/qrels/pass-qrels.msmarco-dev.small.tsv",
+                    "BM25+QLM-T5 and BM25"
+                ],
+                [
+                    "/Users/hangli/ielab/GPT_Ranker/traditional/rerank/t5_rerank_Dirichlet_1000-1_lower.trec.res",
+                    "/Users/hangli/ielab/GPT_Ranker/traditional/first_stage/runs/pass-dev-qld.small.txt",
+                    "/Users/hangli/ielab/GPT_Ranker/data/pass_rerank/qrels/pass-qrels.msmarco-dev.small.tsv",
+                    "QLM-D+QLM-T5 and QLM-D"
+                ],
+                [
+                    "/Users/hangli/ielab/GPT_Ranker/traditional/rerank/t5_rerank_JelinekMercer_1000-1_lower.trec.res",
+                    "/Users/hangli/ielab/GPT_Ranker/traditional/first_stage/runs/pass-dev-qljm.small.txt",
+                    "/Users/hangli/ielab/GPT_Ranker/data/pass_rerank/qrels/pass-qrels.msmarco-dev.small.tsv",
+                    "QLM-JM+QLM-T5 and QLM-JM"
+                ]
+            ]
 
-    dic1 = {}
-    dic2 = {}
-    dic3 = {}
+    fig, axes = plt.subplots(nrows=1, ncols=3, sharey=True, figsize=(18, 4.5))
 
-    with open(fname3, "r") as f3:
-        lines3 = f3.readlines()
-        for line3 in lines3:
-            ss = line3.rstrip().split("\t")
-            if ss[0] not in dic3:
-                dic3[ss[0]] = ss[2]
-            else:
-                continue
-    f3.close()
+    for set_num, paths in enumerate(files):
 
-    with open(fname1, "r") as f1:
-        lines1 = f1.readlines()
-        for linee in tqdm(lines1, desc="Constructing Dictionary 1..."):
-            segs = linee.rstrip().split(" ")
-            if segs[0] not in dic1 and segs[2] == dic3[segs[0]]:
-                dic1[segs[0]] = {
-                    segs[2]: int(segs[3]) - 1
-                }
-            else:
-                continue
-    f1.close()
+        fname1 = paths[0]
+        fname2 = paths[1]
+        fname3 = paths[2]
 
-    with open(fname2, "r") as f2:
-        lines2 = f2.readlines()
-        for linne in tqdm(lines2, desc="Constructing Dictionary 2..."):
-            s = linne.rstrip().split(" ")
-            if s[0] not in dic2 and s[2] == dic3[s[0]]:
-                dic2[s[0]] = {
-                    s[2]: int(s[3]) - 1
-                }
-            else:
-                continue
-    f2.close()
+        dic1 = {}
+        dic2 = {}
+        dic3 = {}
 
-    topics = dic3.keys()
+        with open(fname3, "r") as f3:
+            lines3 = f3.readlines()
+            for line3 in lines3:
+                ss = line3.rstrip().split("\t")
+                if ss[0] not in dic3:
+                    dic3[ss[0]] = ss[2]
+                else:
+                    continue
+        f3.close()
 
-    gainDict = {}
+        with open(fname1, "r") as f1:
+            lines1 = f1.readlines()
+            for linee in tqdm(lines1, desc="Constructing Dictionary 1..."):
+                segs = linee.rstrip().split(" ")
+                if segs[0] not in dic1 and segs[2] == dic3[segs[0]]:
+                    dic1[segs[0]] = {
+                        segs[2]: int(segs[3]) - 1
+                    }
+                else:
+                    continue
+        f1.close()
 
-    for topic in tqdm(topics, desc="Calculating Gain..."):
-        if topic in dic1 and topic in dic2:
-            gain = list(dic1[topic].values())[0] - list(dic2[topic].values())[0]
-            gainDict[topic] = gain
+        with open(fname2, "r") as f2:
+            lines2 = f2.readlines()
+            for linne in tqdm(lines2, desc="Constructing Dictionary 2..."):
+                s = linne.rstrip().split(" ")
+                if s[0] not in dic2 and s[2] == dic3[s[0]]:
+                    dic2[s[0]] = {
+                        s[2]: int(s[3]) - 1
+                    }
+                else:
+                    continue
+        f2.close()
 
-    orderedDict = OrderedDict(sorted(gainDict.items(), key=lambda t: t[1], reverse=True))
+        topics = dic3.keys()
 
-    ind = np.arange(len(list(orderedDict.values())))
+        gainDict = {}
 
-    fname1 = fname1.rsplit("/", 1)[1]
-    fname2 = fname2.rsplit("/", 1)[1]
+        for topic in tqdm(topics, desc="Calculating Gain..."):
+            if topic in dic1 and topic in dic2:
+                gain = list(dic1[topic].values())[0] - list(dic2[topic].values())[0]
+                gainDict[topic] = -gain
 
-    fname1 = fname1.rsplit(".", 1)[0]
-    fname2 = fname2.rsplit(".", 1)[0]
+        orderedDict = OrderedDict(sorted(gainDict.items(), key=lambda t: t[1], reverse=True))
 
-    # plt.xticks(ind, list(gainDict.keys()), rotation="vertical")
-    plt.bar(ind, list(orderedDict.values()))
-    plt.ylim(-topK, topK)
-    plt.title('{} Beats {} By How Much'.format(fname2, fname1))
-    plt.ylabel("Ranked Up")
-    plt.tight_layout()
-    plt.show()
+        ind = np.arange(len(list(orderedDict.values())))
+
+        if set_num == 0:
+            axes[set_num].set_ylabel("Rank Gain", fontsize=17)
+        axes[set_num].bar(ind, list(orderedDict.values()))
+        axes[set_num].set_ylim([-1000, 1000])
+        axes[set_num].set_title(paths[3], fontsize=17)
+        axes[set_num].tick_params(axis='y', labelsize=14)
+        axes[set_num].tick_params(axis='x', labelsize=14)
+    plt.subplots_adjust(left=0.07, bottom=0.1, right=0.98, top=0.9, wspace=0.06)
+    plt.savefig('/Users/hangli/ielab/GPT_Ranker/images/comparison.png')
+    # plt.show()
